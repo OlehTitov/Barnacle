f9ea5b248c4bdaca5285b815bdc32ba36b66ddf9
Oleh Titov <oleg.titov81@gmail.com>
2026-02-23 08:41:52 +0100

    Initial Commit

---
+++ Barnacle/BarnacleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarnacleApp.swift
+//  Barnacle
+//
+//  Created by Oleh Titov on 23.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct BarnacleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
